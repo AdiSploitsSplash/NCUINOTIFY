8d98b3e65f912aed32c20871453e63a207ac4acf
AdiSploitsSplash <79107363+AdiSploitsSplash@users.noreply.github.com>
2022-11-12 22:45:26 +0200

    Update and rename README.md to InjectNCNotify.lua

---
+++ InjectNCNotify.lua
@@ -0,0 +1,10 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/AdiSploitsSplash/NOTIFY/main/notifier.lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/AdiSploitsSplash/notifiy/main/notify.lua"))()
+
+wait(1)
+print("Nitronic Injected!")
+Notification:Notify(
+   {Title = "Nitronic", Description = "Nitronic Injected!"},
+   {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
+   {Image = "http://www.roblox.com/asset/?id=11543390040", ImageColor = Color3.fromRGB(134, 132, 139)}
+)
